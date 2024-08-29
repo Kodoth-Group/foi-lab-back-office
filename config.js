197fe794ef1d0050d40663b19861e8cf281a75c9
Mithun Kodoth <mithunpm809@gmail.com>
2024-08-29 07:41:22 +0400

    [WIP] UI in Next.JS

--- config.js
+++ config.js
@@ -1,27 +1,7 @@
-var path = require("path");
-
-var root = path.join(__dirname);
-
-var config = {
-  rootDir: root,
-  // Targets ========================================================
-  serveDir: path.join(root, ".serve"),
-  distDir: path.join(root, "dist"),
-  clientManifestFile: "manifest.webpack.json",
-  clientStatsFile: "stats.webpack.json",
-
-  // Source Directory ===============================================
-  srcDir: path.join(root, "app"),
-  srcServerDir: path.join(root, "server"),
-
-  // HTML Layout ====================================================
-  srcHtmlLayout: path.join(root, "app", "index.html"),
-
-  // Site Config ====================================================
-  siteTitle: "Airframe",
-  siteDescription: "Default Dashboard ready for Development",
-  siteCannonicalUrl: "http://localhost:4100",
-  siteKeywords: "react dashboard seed bootstrap",
+export default {
+    // Site Config ====================================================
+    siteTitle:              'Airframe',
+    siteDescription:        'Airframe Dashboard ready for Development',
+    siteCannonicalUrl:      'http://localhost:4110',
+    siteKeywords:           'react dashboard seed bootstrap'
 };
-
-module.exports = config;
